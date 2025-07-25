6f929fc34f24a496d464a916994fe0ea86572e55
Bozhan Liang <lbz@rivensbane.com>
2025-07-25 14:53:28 +1200

    init backend

---
+++ src/main/java/com/example/assessment_1/backend/Settings.java
@@ -0,0 +1,13 @@
+package com.example.assessment_1.backend;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+public class Settings {
+
+    @NonNull
+    @Getter
+    @Setter
+    private static String databaseLocation = "";
+}
---
+++ src/main/java/com/example/assessment_1/backend/Student.java
@@ -0,0 +1,5 @@
+package com.example.assessment_1.backend;
+
+public class Student extends Person implements IStudent {
+
+}
