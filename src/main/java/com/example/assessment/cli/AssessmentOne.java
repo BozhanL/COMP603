0fb24e1287273a392d48457813750d174c9556ef
Ben <bensamountry1@gmail.com>
2025-08-05 14:14:28 +1200

    WIP

--- src/main/java/com/example/assessment/cli/AssessmentOne.java
+++ src/main/java/com/example/assessment/cli/AssessmentOne.java
@@ -1,7 +1,25 @@
 package com.example.assessment.cli;
 
+import com.example.assessment.backend.IAuthenticatable;
+import com.example.assessment.backend.IPersonBackend;
+import com.example.assessment.backend.Person;
+import com.example.assessment.backend.PersonFileBackend;
+import java.util.Scanner;
+
 public class AssessmentOne {
 
     public static void main(String[] args) {
+        System.out.println("Welcome"); // use ascii art
+
+        // user inputs database location, or chooses default
+        IPersonBackend pb = new PersonFileBackend();
+
+        // user login
+        String id = "";
+        Person p = pb.getPersonById(id);
+        if (p instanceof IAuthenticatable auth) {
+            auth.safeCheckPassword("pass");
+        }
+
     }
 }
