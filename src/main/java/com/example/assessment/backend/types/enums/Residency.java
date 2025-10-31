9060b41c287c5e04b5cb38a0dfa53af90672c333
Bozhan Liang <lbz@rivensbane.com>
2025-10-31 15:41:47 +1300

    Update enum

--- src/main/java/com/example/assessment/backend/types/enums/Residency.java
+++ src/main/java/com/example/assessment/backend/types/enums/Residency.java
@@ -1,6 +1,9 @@
 package com.example.assessment.backend.types.enums;
 
+import com.google.errorprone.annotations.CheckReturnValue;
+
 // This contains residency status for student
+@CheckReturnValue
 public enum Residency {
     DOMESTIC, INTERNATIONAL
 }
--- src/main/java/com/example/assessment/backend/types/enums/UserType.java
+++ src/main/java/com/example/assessment/backend/types/enums/UserType.java
@@ -1,6 +1,9 @@
 package com.example.assessment.backend.types.enums;
 
+import com.google.errorprone.annotations.CheckReturnValue;
+
 // This contains user type for person
+@CheckReturnValue
 public enum UserType {
     STUDENT, MANAGER
 }
